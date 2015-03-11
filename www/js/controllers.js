5806784248df96ee0c4e6a9edb970f20b6d1579b
markelor <mendimarkel@gmail.com>
2015-03-11 12:13:30 +0100

    Listado de cesta

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,14 +8,72 @@ angular.module('starter.controllers', [])
     Chats.remove(chat);
   };
 })
-//listado de cesta controller
-.controller('ListadoCestaCtrl', function($scope, Productos) {
- /*$scope.shouldShowDelete = false;
- $scope.shouldShowReorder = false;
- $scope.listCanSwipe = false;*/
- $scope.productos=Productos.all();
-})
 
+//listado de cesta controller
+.controller('ListadoCestaCtrl', function($scope,$ionicSideMenuDelegate,Productos) {
+
+  $scope.toggleLeft = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  $scope.data = {
+    showDelete: false
+  };
+  //editar
+  
+  $scope.edit = function(producto) {
+    alert('Edit producto: ' + producto.id);
+  };
+  //compartir
+  $scope.share = function(producto) {
+    alert('Share producto: ' + producto.id);
+  };
+  //mover
+  
+  $scope.moveItem = function(producto, fromIndex, toIndex) {
+    $scope.productos.splice(fromIndex,1);
+    $scope.productos.splice(toIndex, 0, producto);
+  };
+  //borrar
+  
+  $scope.onItemDelete = function(producto) {
+    $scope.productos.splice($scope.productos.indexOf(producto), 1);
+  };
+  //ver productos
+ $scope.productos=Productos.all();
+ })
+ /*
+
+ // Triggered on a button click, or some other target
+ $scope.show = function(producto) {
+
+   // Show the action sheet
+   var hideSheet = $ionicActionSheet.show({
+     producto: [
+       { text: '<b>Share</b> This' },
+       { text: 'Move' }
+     ],
+     destructiveText: 'Delete',
+     titleText: 'Modify your album',
+     cancelText: 'Cancel',
+     cancel: function() {
+          // add cancel code..
+        },
+     buttonClicked: function(index) {
+       return true;
+     }
+   });
+
+   // For example's sake, hide the sheet after two seconds
+   $timeout(function() {
+     hideSheet();
+   }, 2000);
+
+ };
+});
+
+
+*/
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
   $scope.chat = Chats.get($stateParams.chatId);
@@ -35,3 +93,5 @@ angular.module('starter.controllers', [])
   };
 
 });
+
+  
