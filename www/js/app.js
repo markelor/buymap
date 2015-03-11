64ce79a494a54b4d52dc3726f582ffaa12f22a96
jaioneirizar <jaioner14@gmail.com>
2015-03-11 14:38:14 +0100

    Cambios index

--- www/js/app.js
+++ www/js/app.js
@@ -120,5 +120,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/tab-dash');
+  $urlRouterProvider.otherwise('/tab/listadoCesta');
 });
