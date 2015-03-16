5a4d3ef46b9916046cc97212926c3c60523dc65e
markelor <mendimarkel@gmail.com>
2015-03-16 16:46:17 +0100

    resolver conflictos

--- www/js/app.js
+++ www/js/app.js
@@ -8,94 +8,92 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.db', 'starter.ajax'])
 
 .run(function($ionicPlatform, AJAX, Db) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 
-  //obtener datos ajax
- 
-  AJAX.cargarDatos(function(datos){
-    
-    Db.addAllProductos(datos);
-  });
+    //obtener datos ajax
 
+    AJAX.cargarDatos(function(datos) {
+        Db.addAllProductos(datos);
+    });
 
 
-  Db.getAllProductos(function(productos){
 
-       // console.log(productos);
+    Db.getAllProductos(function(productos) {
 
-  });
+        // console.log(productos);
+
+    });
 
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: "/tab",
+        abstract: true,
+        templateUrl: "templates/tabs.html"
+    })
 
-  // Each tab has its own nav history stack:
+    // Each tab has its own nav history stack:
 
-  .state('tab.misListas', {
-    url: '/misListas',
-    views: {
-      'misListas': {
-        templateUrl: 'templates/misListas.html',
-        controller: 'MisListasCtrl',
-         css: 'css/misListas.css'
-      }
-    }
-  })
+    .state('tab.misListas', {
+        url: '/misListas',
+        views: {
+            'misListas': {
+                templateUrl: 'templates/misListas.html',
+                controller: 'MisListasCtrl',
+                css: 'css/misListas.css'
+            }
+        }
+    })
 
- .state('tab.ruta', {
-    url: '/ruta',
-    views: {
-      'ruta': {
-        templateUrl: 'templates/ruta.html',
-        controller: 'RutaCtrl'
-      }
-    }
-  })
+    .state('tab.ruta', {
+        url: '/ruta',
+        views: {
+            'ruta': {
+                templateUrl: 'templates/ruta.html',
+                controller: 'RutaCtrl'
+            }
+        }
+    })
 
 
 
- .state('tab.registro', {
-    url: '/registro',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/registro2.html',
-        controller: 'RegistroCtrl'
-      }
-    }
-  })
- .state('tab.login', {
-    url: '/login',
-    views: {
-      'tab-login': {
-        templateUrl: 'templates/login.html',
-        controller: 'LoginCtrl'
-      }
-    }
-  });
+    .state('tab.registro', {
+            url: '/registro',
 
-  // ir a esta ruta por defecto
-  $urlRouterProvider.otherwise('/tab/login');
+
+            templateUrl: 'templates/registro2.html',
+            controller: 'RegistroCtrl'
+
+
+        })
+        .state('login', {
+            url: '/login',
+
+            templateUrl: 'templates/login.html',
+            controller: 'LoginCtrl'
+
+
+        });
+
+    // ir a esta ruta por defecto
+    $urlRouterProvider.otherwise('/tab/login');
 });
